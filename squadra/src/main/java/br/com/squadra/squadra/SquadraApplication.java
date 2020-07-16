88f8067b5d353e27dc422046dc5d9f46fadeff89
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-07-16 10:50:01 -0300

    Arrumei o erro

--- squadra/src/main/java/br/com/squadra/squadra/SquadraApplication.java
+++ squadra/src/main/java/br/com/squadra/squadra/SquadraApplication.java
@@ -1,17 +1,16 @@
 package br.com.squadra.squadra;
 
+
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import br.com.squadra.controller.SistemaController;
 
-@EnableJpaRepositories("br.com.squadra.repository")
-@EnableAutoConfiguration
+
+
+
+
 @SpringBootApplication
-@ComponentScan(basePackageClasses = SistemaController.class)
 public class SquadraApplication {
 
 	public static void main(String[] args) {
